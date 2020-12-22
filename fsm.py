3a0c7c141f275231a117ecfa1ec7f5ab408bed78
huanjing1218 <huanjing881218@gmail.com>
2020-12-22 22:48:09 +0800

    Test button template

--- fsm.py
+++ fsm.py
@@ -1,29 +1,29 @@
 from transitions.extensions import GraphMachine
 
-from utils import send_text_message
+from utils import send_text_message, send_menu
 
 
 class TocMachine(GraphMachine):
     def __init__(self, **machine_configs):
         self.machine = GraphMachine(model=self, **machine_configs)
 
-    def is_going_to_state1(self, event):
+    def is_going_to_weather_service(self, event):
         text = event.message.text
-        return text.lower() == "go to state1"
+        return text.lower() == "天氣"
 
     def is_going_to_state2(self, event):
         text = event.message.text
         return text.lower() == "go to state2"
 
-    def on_enter_state1(self, event):
-        print("I'm entering state1")
+    def on_enter_weather_service(self, event):
+        print("I'm entering Weather Service")
 
         reply_token = event.reply_token
-        send_text_message(reply_token, "Trigger state1")
+        send_menu(reply_token)
         self.go_back()
 
-    def on_exit_state1(self):
-        print("Leaving state1")
+    def on_exit_weather_service(self):
+        print("Leaving Weather Service")
 
     def on_enter_state2(self, event):
         print("I'm entering state2")
--- utils.py
+++ utils.py
@@ -1,7 +1,7 @@
 import os
 
 from linebot import LineBotApi, WebhookParser
-from linebot.models import MessageEvent, TextMessage, TextSendMessage
+from linebot.models import MessageEvent, TextMessage, TextSendMessage, ButtonsTemplate
 
 
 channel_access_token = os.getenv("LINE_CHANNEL_ACCESS_TOKEN", None)
@@ -13,11 +13,31 @@ def send_text_message(reply_token, text):
 
     return "OK"
 
+def send_menu(reply_token):
+    line_bot_api = LineBotApi(channel_access_token)
+    message = TemplateSendMessage(
+        alt_text='Buttons template',
+        template=ButtonsTemplate(
+            title='天氣服務',
+            text='請點選需要的服務',
+            actions=[
+                MessageTemplateAction(
+                    label='weather',
+                    text='即時天氣'
+                ),
+                MessageTemplateAction(
+                    label='forecast',
+                    text='天氣預報'
+                )
+            ]
+        )
+    )
+    line_bot_api.reply(reply_token, message)
+    return
 
 """
 def send_image_url(id, img_url):
     pass
-
 def send_button_message(id, text, buttons):
     pass
 """
