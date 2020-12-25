25472613706658b73b7450f78874f10bc17bc6ec
huanjing1218 <huanjing881218@gmail.com>
2020-12-25 14:39:04 +0800

    Add weather forecast and radar echo

--- fsm.py
+++ fsm.py
@@ -1,27 +1,27 @@
 from transitions.extensions import GraphMachine
 
-from utils import send_text_message, send_menu, query_realtime_weather
+from utils import send_text_message, send_menu, check_weather, check_forecast, radar_echo
 
 
 class TocMachine(GraphMachine):
     def __init__(self, **machine_configs):
         self.machine = GraphMachine(model=self, **machine_configs)
 
-    def is_going_to_weather_service(self, event):
+    def choose_weather_service(self, event):
         text = event.message.text
         return text.lower() == "天氣"
     
-    def is_going_to_search_realtime(self, event):
+    def is_going_to_check_weather(self, event):
         text = event.message.text
         return text.lower() == "即時天氣"
 
-    def is_going_to_search_forecast(self, event):
+    def is_going_to_check_forecast(self, event):
         text = event.message.text
         return text.lower() == "天氣預報"
 
-    def is_going_to_state2(self, event):
+    def is_going_to_radar_echo(self, event):
         text = event.message.text
-        return text.lower() == "go to state2"
+        return text.lower() == "雷達回波"
 
     def on_enter_weather_service(self, event):
         print("I'm entering Weather Service")
@@ -29,38 +29,34 @@ class TocMachine(GraphMachine):
         send_menu(reply_token)
         return "OK"
 
-    def on_enter_search_realtime(self, event):
+    def on_enter_check_weather(self, event):
         print("I'm entering Search Realtime")
         reply_token = event.reply_token
         send_text_message(reply_token, "請輸入縣市以及行政區\n(例如：台南市中西區)")
         return "OK"
 
-    def on_enter_search_forecast(self, event):
+    def on_enter_check_forecast(self, event):
         print("I'm entering Search Forecast")
         reply_token = event.reply_token
         send_text_message(reply_token, "請輸入縣市以及行政區\n(例如：台南市中西區)")
         return "OK"  
 
-    def on_enter_realtime_weather(self, event):
-        print("I'm entering Realtime Weather")
+    def on_enter_radar_echo(self, event):
+        print("I'm entering Radar")
+        reply_token = event.reply_token
+        radar_echo(reply_token)
+        self.go_back() 
+
+    def on_enter_weather(self, event):
+        print("I'm entering Weather")
         text = event.message.text
         reply_token = event.reply_token
-        send_text_message(reply_token,  query_realtime_weather(text))
+        send_text_message(reply_token, check_weather(text))
         self.go_back()
 
     def on_enter_weather_forecast(self, event):
         print("I'm entering Weather Forecast")
         text = event.message.text 
         reply_token = event.reply_token
-        send_text_message(reply_token, "預報結果")
+        send_text_message(reply_token, check_forecast(text))
         self.go_back()
-
-    def on_enter_state2(self, event):
-        print("I'm entering state2")
-
-        reply_token = event.reply_token
-        send_text_message(reply_token, "Test in state2 123123")
-        self.go_back()
-
-    def on_exit_state2(self):
-        print("Leaving state2")
--- utils.py
+++ utils.py
@@ -2,17 +2,20 @@ import os
 import requests
 import json
 
+from dotenv import load_dotenv
 from linebot import LineBotApi, WebhookParser
-from linebot.models import MessageEvent, TextMessage, TextSendMessage, ButtonsTemplate, TemplateSendMessage, MessageTemplateAction
+from linebot.models import MessageEvent, TextMessage, TextSendMessage, ButtonsTemplate, TemplateSendMessage, MessageTemplateAction, ImageSendMessage
 
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from bs4 import BeautifulSoup
 
+load_dotenv()
 channel_access_token = os.getenv("LINE_CHANNEL_ACCESS_TOKEN", None)
-
 
 def send_text_message(reply_token, text):
     line_bot_api = LineBotApi(channel_access_token)
     line_bot_api.reply_message(reply_token, TextSendMessage(text=text))
-
     return "OK"
 
 def send_menu(reply_token):
@@ -41,18 +44,19 @@ def send_menu(reply_token):
     line_bot_api.reply_message(reply_token, message)
     return "OK"
 
-def query_realtime_weather(location):
+def check_weather(location):
+    location = location.replace(' ', '')
     city = location[0:3]
     town = location[3:]
     city = city.replace('台', '臺')
     result = ''
     find = False
 
-    end_point = ['https://opendata.cwb.gov.tw/api/v1/rest/datastore/O-A0003-001?Authorization=CWB-56562D80-292A-4ED6-BA0C-F6B5A1B82538&format=JSON', 'https://opendata.cwb.gov.tw/api/v1/rest/datastore/O-A0001-001?Authorization=CWB-56562D80-292A-4ED6-BA0C-F6B5A1B82538&format=JSON']
-    for i in end_point:
+    website = ['https://opendata.cwb.gov.tw/api/v1/rest/datastore/O-A0003-001?Authorization=CWB-56562D80-292A-4ED6-BA0C-F6B5A1B82538&format=JSON', 'https://opendata.cwb.gov.tw/api/v1/rest/datastore/O-A0001-001?Authorization=CWB-56562D80-292A-4ED6-BA0C-F6B5A1B82538&format=JSON']
+    for i in website:
         if find:
             break
-        data = requests.get(end_point).json()
+        data = requests.get(i).json()
         data = data['records']['location']
         for j in data:
             if city == j['parameter'][0]['parameterValue'] and town == j['parameter'][2]['parameterValue']:
@@ -66,12 +70,64 @@ def query_realtime_weather(location):
                 break
 
     if not find:
-        result = '查無結果\n請確認輸入是否有誤或改輸入鄰近行政區'
+        result = '查無結果\n請確認輸入內容或輸入鄰近行政區'
     return result      
 
+def check_forecast(location):
+    location = location.replace(' ', '')
+    city = location[0:3]
+    town = location[3:]
+    city = city.replace('台', '臺')
+    result = ''
+    taiwan = {'臺北市': '063', '新北市': '071', '基隆市': '051', '桃園市': '007', '新竹縣': '011', '新竹市': '055', '苗栗縣': '015', '臺中市': '075', '南投縣': '023', '彰化縣': '019', '雲林縣': '027', '嘉義縣': '031', '嘉義市': '059', '臺南市': '079', '高雄市': '067', '屏東縣': '035', '宜蘭縣': '003', '花蓮縣': '043', '臺東縣': '039', '澎湖縣': '047', '金門縣': '087', '連江縣': '083'}
+    if city in taiwan:
+        ID = taiwan.get(city)
+        result += city + town + '　三天天氣預報'
+
+        website = 'https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-D0047-' + ID + '?Authorization=CWB-56562D80-292A-4ED6-BA0C-F6B5A1B82538&elementName=WeatherDescription'
+        data = requests.get(website).json()
+        data = data['records']['locations'][0]['location']
+        find = False
+        for i in range(len(data)):
+            if town == data[i]['locationName']:
+                print(len(data[i]['locationName']))
+                data = data[i]['weatherElement'][0]['time']
+                find = True
+                break
+        if find:
+            for i in range(6):
+                result += '\n\n' + data[i]['startTime'] + '~' + data[i]['endTime'][-8:]
+                tmp = data[i]['elementValue'][0]['value'].split('。')
+                for j in range(3):
+                    result += '\n' + tmp[j]
+        else:
+            result = '查無結果\n請確認輸入內容'
+    else:
+        result = '查無結果\n請確認輸入內容'
+    return result
+
+def radar_echo(reply_token):
+    options = Options()
+    options.add_argument("--disable-notifications")
+    chrome = webdriver.Chrome('./chromedriver', chrome_options=options)
+    chrome.get("https://www.cwb.gov.tw/V8/C/W/OBS_Radar.html")
+    soup = BeautifulSoup(chrome.page_source, "html.parser")
+    result = soup.find_all("img", {"alt": "雷達回波"})
+
+    website = []
+    for i in result:
+        website.append('https://www.cwb.gov.tw' + str(i.get("src")))
+    chrome.close()
+
+    line_bot_api = LineBotApi(channel_access_token)
+    message = ImageSendMessage(
+            original_content_url = website[0],
+            preview_image_url = website[0]
+    )
+    line_bot_api.reply_message(reply_token, message)
+    return "OK"
+
 """
-def send_image_url(id, img_url):
-    pass
 def send_button_message(id, text, buttons):
     pass
 """
